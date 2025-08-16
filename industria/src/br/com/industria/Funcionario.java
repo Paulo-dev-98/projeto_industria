fcc7d784073aba898e10d887ee076e7aa46477d2
Paulo-dev-98 <paulobg8@gmail.com>
2025-08-16 17:52:05 -0300

    exercicio 1 e 2

--- industria/src/br/com/industria/Funcionario.java
+++ industria/src/br/com/industria/Funcionario.java
@@ -1,5 +1,60 @@
 package br.com.industria;
 
-public class Funcionario {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+	
+	BigDecimal salario;
+	String funcao;
+	
+	public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+		super(nome, dataNascimento);
+		this.salario = salario;
+		this.funcao = funcao;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+	public String getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((funcao == null) ? 0 : funcao.hashCode());
+		result = prime * result + ((salario == null) ? 0 : salario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (funcao == null) {
+			if (other.funcao != null)
+				return false;
+		} else if (!funcao.equals(other.funcao))
+			return false;
+		if (salario == null) {
+			if (other.salario != null)
+				return false;
+		} else if (!salario.equals(other.salario))
+			return false;
+		return true;
+	}
 
 }
--- industria/src/br/com/industria/Pessoa.java
+++ industria/src/br/com/industria/Pessoa.java
@@ -1,5 +1,60 @@
 package br.com.industria;
 
+import java.time.LocalDate;
+
 public class Pessoa {
+	
+	String nome;
+	LocalDate dataNascimento;
+	
+	public Pessoa(String nome, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (dataNascimento == null) {
+			if (other.dataNascimento != null)
+				return false;
+		} else if (!dataNascimento.equals(other.dataNascimento))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 
 }
